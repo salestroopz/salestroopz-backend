439fa3787274ebf1e428a316b8c6ea655c032998
salestroopz <haricharansm@gmail.com>
2025-04-12 21:42:29 -0500

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,9 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    app_name: str = "Salestroopz"
+    admin_email: str = "admin@salestroopz.com"
+    environment: str = "development"
+
+    class Config:
+        env_file = ".env"
