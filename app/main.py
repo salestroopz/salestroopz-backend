673a4bd0fc5b9c122799d8f77594a8a70ed10102
salestroopz <haricharansm@gmail.com>
2025-04-15 13:00:19 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -28,3 +28,5 @@ from app.agents import datalist  # make sure import path is correct
 @app.post("/generate-prospects")
 def generate_prospects(data: datalist.ProspectRequest):
     return datalist.generate_prospect_data(data)
+from app.routes import agents  # add this
+app.include_router(agents.router)  # and this
