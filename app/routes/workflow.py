cb190298c6ab84936b5a18c79874d760601c19b2
salestroopz <haricharansm@gmail.com>
2025-04-16 21:57:35 -0500

    Update workflow.py

--- app/routes/workflow.py
+++ app/routes/workflow.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
-from app.schemas import ICPModel
+from app.schemas import ICPRequest
 from app.agents.leadworkflow import LeadWorkflowAgent
 
 router = APIRouter()
 
 @router.post("/full-cycle")
-def run_full_cycle_workflow(icp: ICPModel):
+def run_full_cycle_workflow(icp: ICPRequest):
     agent = LeadWorkflowAgent()
     return agent.run_full_workflow(icp)
